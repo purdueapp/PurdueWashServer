488c1153e3f388dd93d0ec23b9ad44cc2b1d6169
moldingtofu <jeremykchang@gmail.com>
2019-03-28 17:57:16 -0400

    room details

--- scrapper.go
+++ scrapper.go
@@ -19,7 +19,7 @@ type room struct {
 type machine struct {
   Name string
   Status string
-  TimeRemaining int32
+  TimeRemaining string
 }
 
 var url = "http://wpvitassuds01.itap.purdue.edu/washalertweb/washalertweb.aspx"
@@ -43,28 +43,71 @@ func getLoc() []room {
 
 func getMachines(dorm room) []machine {
   var machines = []machine{}
+  var availWash int32 = 0
+  var availDry int32 = 0
+  var wash int32 = 0
+  var dry int32 = 0
 
   c := colly.NewCollector()
   c.OnHTML("tr.MachineReadyMode", func(e *colly.HTMLElement) {
     temp := machine{}
     temp.Name = e.ChildText("td.name")
     temp.Status = e.ChildText("td.status")
-    //temp.TimeRemaining = e.ChildText("time")
+    temp.TimeRemaining = e.ChildText("time")
     machines = append(machines, temp)
+
+    if e.ChildText("td.type") == "Dryer" {
+      availDry++
+      dry++
+    } else {
+      availWash++
+      wash++
+    }
   })
 
   c.OnHTML("MachineRunMode", func(e *colly.HTMLElement) {
+    temp := machine{}
+    temp.Name = e.ChildText("td.name")
+    temp.Status = e.ChildText("td.status")
+    temp.TimeRemaining = e.ChildText("time")
+    machines = append(machines, temp)
+
+    if e.ChildText("td.type") == "Dryer" {
+      dry++
+    } else {
+      wash++
+    }
   })
 
   c.OnHTML("MachineEndOfCycle", func(e *colly.HTMLElement) {
+    temp := machine{}
+    temp.Name = e.ChildText("td.name")
+    temp.Status = e.ChildText("td.status")
+    temp.TimeRemaining = e.ChildText("time")
+    machines = append(machines, temp)
+
+    if e.ChildText("td.type") == "Dryer" {
+      dry++
+    } else {
+      wash++
+    }
   })
 
   c.OnHTML("MachineRunModeAlmostDone", func(e *colly.HTMLElement) {
+    temp := machine{}
+    temp.Name = e.ChildText("td.name")
+    temp.Status = e.ChildText("td.status")
+    temp.TimeRemaining = e.ChildText("time")
+    machines = append(machines, temp)
+
+    if e.ChildText("td.type") == "Dryer" {
+      dry++
+    } else {
+      wash++
+    }
   })
 
   c.Visit(url + dorm.Url)
-
-  fmt.Println(machines)
 
   return machines
 }
@@ -73,7 +116,7 @@ func main() {
   var rooms = getLoc()
   for _, room := range rooms {
     room.Machines = getMachines(room)
+    fmt.Println(room.Machines)
   }
 
-  fmt.Println(rooms)
 }
--- test.go
+++ test.go
@@ -10,9 +10,20 @@ var url = "http://wpvitassuds01.itap.purdue.edu/washalertweb/washalertweb.aspx"
 func main() {
   c := colly.NewCollector()
 
-  c.OnHTML("tr.MachineReadyMode", func(e *colly.HTMLElement) {
+//  c.OnHTML("tr.MachineReadyMode", func(e *colly.HTMLElement) {
+//    fmt.Println(e.ChildText("td.name"))
+//    fmt.Println(e.ChildText("td.status"))
+//    fmt.Println(e.ChildText("td.time"))
+//  })
+
+  var count int32 = 0
+  c.OnHTML("tr.MachineRunMode", func(e *colly.HTMLElement) {
     fmt.Println(e.ChildText("td.name"))
+    fmt.Println(e.ChildText("td.status"))
+    fmt.Println(e.ChildText("td.time"))
+    count++
   })
 
   c.Visit(url)
+  fmt.Println(count)
 }
