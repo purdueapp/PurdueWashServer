c4db86f9a1537f41c82fd03b637dcfd4636913b7
Tobi Ola <oluwatobi.ola@gmail.com>
2019-03-28 20:52:34 -0400

    basic server response working

--- server.go
+++ server.go
@@ -3,36 +3,50 @@ package main
 import (
   "net/http"
   "encoding/json"
-  "fmt"
-  "github.com/bxcodec/faker"
+//  "fmt"
 )
 
 type LaundryRoom struct {
-  Name string
-  Url string
-  ImageUrl string
-  AvailableWashers int32
-  TotalWashers int32
-  AvailableDryers int32
-  TotalDryers int32
-  Machines []Machine
+        Name                string    `json:"name"`
+        Url                 string    `json:"url"`
+        AvailableWashers    int32     `json:"availableWashers"`
+        TotalWashers        int32     `json:"totalWashers"`
+        AvailableDryers     int32     `json:"availableDryers"`
+        TotalDryers         int32     `json:"totalDryers"`
+        Machines            []Machine `json:"machines"`
 }
 
 type Machine struct {
-  Name string
-  Status string
-  TimeRemaining int32
+        Name          string  `json:"name"`
+        Status        string  `json:"status"`
+        TimeRemaining int32   `json:"timeRemaining"`
 }
 
 func req(w http.ResponseWriter, r *http.Request) {
 
   data := LaundryRoom{}
-  faker.FakeData(&data)
-  fmt.Println(json.Marshal(data))
-  fmt.Fprint(w, data)
+  data.Name = "Wiley"
+  data.AvailableWashers = 43
+  data.TotalWashers = 23
+  data.AvailableDryers = 53
+  data.TotalDryers = 12
+
+  machines := []Machine{}
+  machines = append(machines, Machine{"Dryer 01", "out of order", 0})
+  machines = append(machines, Machine{"Dryer 02", "available", 0})
+  machines = append(machines, Machine{"Dryer 03", "in use", 5})
+  machines = append(machines, Machine{"Dryer 04", "end of cycle", 0})
+  machines = append(machines, Machine{"Dryer 05", "out of order", 0})
+  machines = append(machines, Machine{"Dryer 06", "out of order", 0})
+
+  data.Machines = machines
+
+  laundryRooms := [1]LaundryRoom{data}
+
+  json.NewEncoder(w).Encode(laundryRooms)
 }
 
 func main() {
   http.HandleFunc("/", req)
-  http.ListenAndServe(":8421", nil)
+  http.ListenAndServe(":8424", nil)
 }
