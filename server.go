52b623614c0e1499f84ca098519cccd223a4425e
moldingtofu <jeremykchang@gmail.com>
2019-04-08 18:31:53 -0400

    server routing?

--- server.go
+++ server.go
@@ -5,12 +5,16 @@ import (
   "encoding/json"
   "fmt"
   "log"
+  "github.com/gorilla/mux"
 )
 
 func main() {
   fmt.Println("Server started and running at port 8421")
 
-  http.HandleFunc("/", req)
+  r := mux.NewRouter()
+  r.HandleFunc("/{room}", RoomHandler).Queries("location", "{link}")
+  r.HandleFunc("/", req)
+  http.Handle("/", r)
   log.Fatal(http.ListenAndServe(":8421", nil))
 }
 
@@ -21,4 +25,15 @@ func req(w http.ResponseWriter, r *http.Request) {
 
   w.Header().Set("Content-Type", "application/json")
   json.NewEncoder(w).Encode(laundryRooms)
+}
+
+func RoomHandler(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  room := Room {
+    Name: vars["room"],
+    Url: "?location=" + vars["link"],
+  }
+  room = GetInfo(room)
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(room)
 }
